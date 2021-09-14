fb0329080f2fe43ccc091b94749847afbdbf7de9
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-14 17:12:14 +0200

    Add SearchesTest:testFindUserFamilyNameByUserNameDistinct #1

--- src/test/java/func_lambda/SearchesTest.java
+++ src/test/java/func_lambda/SearchesTest.java
@@ -1,4 +1,20 @@
 package func_lambda;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SearchesTest {
+
+    @Test
+    void testFindUserFamilyNameByUserNameDistinct(){
+        assertEquals(
+                List.of("Torres"),
+                new Searches().findUserFamilyNameByUserNameDistinct("Paula")
+                        .collect(Collectors.toList())
+        );
+    }
 }
